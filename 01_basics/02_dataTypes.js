6664d26c278a9ca7932fa799c83efeda8de5b0f5
lavkushhh123 <lavkushhelium@gmail.com>
2026-09-06 15:37:59 +0530

    01basics/01dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict" ;       //treate all js code as newer version
+
+// alert( 3 +3 )        // there is different in node to use alert , so can't use right now 
+
+console.log( 3 + 3)     // codde readibility should be high
+
+console.log( "Lavkush")
+
+
+let name = "lavkush"
+let age = 18
+let isLoggedIn = false
+
+// number  => rannge 2 to the power 53
+
+// bigint   for largge no.
+// string  => ""
+// boolean => true / false
+// null   => stand alone value, i.e is keep the value as null
+// undefined => means value is not assigned yet
+// symbol => used for uniqueness
+
+// object
+
+console.log(typeof "lavkush")
+console.log(typeof null)      // isak type object hai
+console.log(typeof undefinded)  // iska type undefined hai
